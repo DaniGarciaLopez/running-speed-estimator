c86fa6d575e6c7399917c3e182aec877bbfb9c0e
kikefdezl <75074147+kikefdezl@users.noreply.github.com>
2020-12-06 19:31:14 +0100

    Add files via upload

---
+++ data_manipulation.py
@@ -0,0 +1,39 @@
+#Requires Matplotlib, numpy and scipy for python
+
+import scipy.signal
+import import_json as ij
+import numpy as np
+import matplotlib.pyplot as plt
+
+kp = ij.get_keypoints()     #kp is a list of all the 25x3 lists of keypoints
+#kp = ij.interpolate_uncertain_points(kp)       #uncomment to interpolate uncertain points
+
+#constants
+eyes=17
+neck=1
+hip=8
+left_knee=13
+right_knee=10
+left_ankle=14
+right_ankle=11
+
+x_values = []
+y_values = []
+
+#imports a series of keypoints and outputs smoothed data
+def smooth_data(kp):
+    kp_filtered = kp
+    for kp_id in range(25):
+        x_values.clear()
+        y_values.clear()
+        for f_kp in kp:
+            x_values.append(f_kp[kp_id][0])
+            y_values.append(f_kp[kp_id][1])
+        x_values_filtered = scipy.signal.savgol_filter(x_values, 111, 2)
+        x_values_filtered = scipy.signal.savgol_filter(x_values_filtered, 51, 3)
+        y_values_filtered = scipy.signal.savgol_filter(y_values, 111, 2)
+        y_values_filtered = scipy.signal.savgol_filter(y_values_filtered, 51, 3)
+        for i in range(len(kp)):
+            kp_filtered[i][kp_id][0]=x_values_filtered[i]
+            kp_filtered[i][kp_id][1]=y_values_filtered[i]
+    return kp_filtered
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 #must have OpenCv, numpy and matplot lib installed for python
 import cv2
 import import_json as ij
+import data_manipulation as dm
 import numpy as np
 import math
 from matplotlib import pyplot as plt
 
 #key data
-athlete_height = 180 # in cm
-camera_fps = 60
+athlete_height = 170 # in cm
+camera_fps = 600
 
 #importing json file data
 kp = ij.get_keypoints()     #kp is a list of all the 25x3 lists of keypoints
+kp = dm.smooth_data(kp)
 #kp = ij.interpolate_uncertain_points(kp)       #uncomment to interpolate uncertain points
 f_kp = []       #f_kp will be a 25x3 list of the keypoints at a specific frame
 
 #init
 speed = 0
-max_distance = 0
+prev_ang = 0
 stride_frames = 0
 prev_ankle_distance = 0
+direction = 1
 
 #constants
 bottom_left_of_screen = (20, 700)
@@ -29,9 +32,10 @@ left_knee=13
 right_knee=10
 left_ankle=14
 right_ankle=11
+is_running = True
 
 #get angle between three points
-def getAngle(a, b, c):
+def get_angle(a, b, c):
     ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
     return ang
 
@@ -50,6 +54,12 @@ def get_pixels_per_meter(f_kp):   #returns how many pixels correlate to 1 meter 
 
     pixels = int(distance/athlete_height*100)
     return pixels
+
+def get_stride_distance(height, is_running):
+    if is_running:
+        return height*0.0125
+    else:
+        return height*0.00413
 
 #function to draw a meter measurement line on the floor using the athlete's height at a specific frame
 def draw_meter_lines(frame, f_kp):
@@ -97,49 +107,43 @@ def draw_athlete_speed(frame, speed):
 
 
 #main
-while(1):
-    cap = cv2.VideoCapture('makau.mp4')
-    frame_count = 0
+cap = cv2.VideoCapture('makau.mp4')
+frame_count = 0
 
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if not ret:
-            break
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-        f_kp = kp[frame_count]
+    f_kp = kp[frame_count]
 
-        frame = draw_keypoints(frame, f_kp)
+    frame = draw_keypoints(frame, f_kp)
 
-        # aqui iria el algoritmo de deteccion de velocidad, esto que he comentado ha sido un intento de hacerlo con la distancia entre tobillos
+    #here is the algorithm that detects velocity
 
-        # hip_coord = (kp[hip][0], kp[hip][1])
-        # left_ankle_coord = (kp[left_ankle][0], kp[left_ankle][1])
-        # right_ankle_coord = (kp[right_ankle][0], kp[right_ankle][1])
-        # ankle_distance = math.fabs(left_ankle_coord[0]-right_ankle_coord[0])
-        # if ankle_distance > prev_ankle_distance-5:
-        #     stride_frames +=1
-        #     prev_ankle_distance = ankle_distance
-        # else:
-        #     stride_pixels = ankle_distance
-        #     stride_meters = stride_pixels/get_pixels_per_meter(kp)
-        #     stride_time = stride_frames*(1/camera_fps)
-        #     try:
-        #         speed = (stride_meters/stride_time)*3.6
-        #     except ZeroDivisionError:
-        #         print ("Division by zero")
-        #     stride_frames=0
-        #     prev_ankle_distance = ankle_distance
-        #     print("The stride travelled " + str(stride_meters) + " meters")
-        #     print("The stride took " + str(stride_time) + " seconds")
-        #     print("The speed is " + str(speed) + " kilometers per hour")
+    hip_coord = (f_kp[hip][0], f_kp[hip][1])
+    left_ankle_coord = (f_kp[left_ankle][0], f_kp[left_ankle][1])
+    right_ankle_coord = (f_kp[right_ankle][0], f_kp[right_ankle][1])
+    ang = direction*get_angle(left_ankle_coord, hip_coord, right_ankle_coord)
 
-        frame = draw_athlete_speed(frame, speed)
+    if ang > prev_ang:
+        stride_frames +=1
+        prev_ang = ang
+    else:
+        direction*=-1
+        prev_ang = -ang
+        stride_meters = get_stride_distance(athlete_height,True)
+        stride_time = stride_frames*(1/camera_fps)
+        speed = (stride_meters/stride_time)*3.6
+        stride_frames=0
 
-        cv2.imshow('frame', frame)
-        frame_count += 1
+    frame = draw_athlete_speed(frame, speed)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            exit()
+    cv2.imshow('frame', frame)
+    frame_count += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
